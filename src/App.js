56024b465d60b7036367e0a9fda24c791eba626d
missvader <vanepalazon611@gmail.com>
2022-11-20 19:19:22 +0100

    Exercise 3. Webservices as a component again. Added buttons to add and substract

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { useState} from 'react';
-/*import WebServices from './components/WebServices';*/
+import WebServices from './components/WebServices';
 import './App.css';
 
 
 function App() {
-  /*
-    (NOTA: version del ejercicio 2 todo junto)
-    (PENDIENTE: separar Webservices component)
-  */
   //HOOKS
   /*Checkboxes, inputs controlados. Creamos un unico estado en formato array para centralizar la situacion de las 3 casillas -> estado inicial false pq no estan checked*/
   const [datos, setDatos] = useState({
@@ -23,17 +19,6 @@ function App() {
   /*esta es la funcion que maneja el evento onChange de los checkbox:
   simplemente maneja el estado ejecutanto el setDatos, que conserva los datos ya existentes con el spread operator y agrega los nuevos datos en funcion de si se selecciona o deseleciona el checkbox */
   const handleInputChange = (event) => {
-    /*const value = parseInt(event.target.value);
-    const {name, checked} = event.target;
-    setDatos(prevDatos =>{
-      return{
-        ...prevDatos,
-        [name]: checked
-      }
-    })
-    checked
-      ? setBudget(budget + value)
-      : setBudget(budget - value);*/
     setDatos({
       ...datos,
       [event.target.name]: !datos[event.target.name],
@@ -45,6 +30,7 @@ function App() {
     const seoPrice = datos.seo ? 300 : 0;
     const googlePrice = datos.google ? 200 : 0;
     const budget = webPrice + seoPrice + googlePrice;
+ 
   return (
     <div className="App">
      <form className='form container border mt-3 p-3'>
@@ -53,7 +39,6 @@ function App() {
         <input 
           className="form-check-input" 
           type="checkbox" 
-          /*value={500} */
           name="web"
           onChange={handleInputChange}
           checked={datos.web}
@@ -63,39 +48,17 @@ function App() {
         </label>
       </div>
       {/*Renderizado condicional de WebServices component(de momento componente dentro de app), simplemente al ejecutar funcion que maneja evento, se recoge value y se actualiza estado de pages y languages*/
-        datos.web && /*<WebServices/>*/
-            (
-              <div className="d-inline-flex p-3 flex-column border border-3 rounded-3 border-dark">
-      <div className="m-2">
-        <label htmlFor="pages" className="p-2">
-          Número de páginas
-        </label>
-        <input type="number"
-              name="numPages"
-              min={1} 
-              value={pages}
-              onChange={(event)=> setPages(event.target.value)}
-          />
-      </div>
-      <div className="m-2">
-        <label htmlFor="languages" className="p-2">
-          Número de idiomas
-        </label>
-        <input type="number" 
-              name="numLanguages"
-              min={1}
-              value={languages}
-              onChange={(event)=> setLanguages(event.target.value)}
-          />
-      </div>
-    </div>
-            )
+        datos.web && <WebServices
+              pages={pages}
+              languages={languages}
+              setPages={setPages}
+              setLanguages ={setLanguages}
+              />
       }
       <div className="form-check mt-2">
         <input 
           className="form-check-input" 
           type="checkbox" 
-          /*value={300}*/
           name="seo"
           checked={datos.seo}
           onChange={handleInputChange}
@@ -108,7 +71,6 @@ function App() {
         <input 
           className="form-check-input" 
           type="checkbox" 
-          /*value={200}*/
           name="google"
           checked={datos.google}
           onChange={handleInputChange}
--- src/components/WebServices.js
+++ src/components/WebServices.js
@@ -1,25 +1,60 @@
 import React from "react";
-function WebServices(){
-
+// PENDIENTE: Refactorizar codigo
+/*al componente le pasamos por parametros languages, pages y los set de cada uno para modificar estado. necesitamos que el componente hijo se comunique con el padre y lo hacemos mediante props */
+//LOGICA
+/*Estas son las funciones que manejan los eventos, tanto input como button (está pendiente refactorizar código) */
+function WebServices({pages, languages, setPages, setLanguages}){
+  const handleChangePages =(event)=>{
+    event.prentDefault();
+    setPages(parseInt(event.target.value));
+  }
+  const handleChangeLanguages=(event)=> {
+    event.preventDefault();
+    setLanguages(parseInt(event.target.value));
+  }
+  const sumarPages = (event)=> {
+    event.preventDefault();
+    setPages(pages => pages + 1)
+  } 
+  const restarPages = (event) => {
+    event.preventDefault();
+    setPages(pages => pages > 1 ? pages - 1 : pages)
+  }
+  const sumarLanguages = (event)=> {
+    event.preventDefault();
+    setLanguages(languages => languages + 1)
+  } 
+  const restarLanguages = (event) => {
+    event.preventDefault();
+    setLanguages(languages => languages > 1 ? languages - 1 : languages)
+  }
   return (
     <div className="d-inline-flex p-3 flex-column border border-3 rounded-3 border-dark">
       <div className="m-2">
         <label htmlFor="pages" className="p-2">
           Número de páginas
         </label>
-        <input type="number"
+        <button onClick={sumarPages}>+</button>
+        <input 
               name="pages"
+              value={pages}
               min={1} 
+              onChange={handleChangePages}
           />
+        <button onClick={restarPages}>-</button>
       </div>
       <div className="m-2">
         <label htmlFor="languages" className="p-2">
           Número de idiomas
         </label>
-        <input type="number" 
-              name="numLanguages"
+        <button onClick={sumarLanguages}>+</button>
+        <input 
+              name="languages"
               min={1}
+              value={languages}
+              onChange= {handleChangeLanguages}
           />
+        <button onClick={restarLanguages}>-</button>
       </div>
     </div>
   );
