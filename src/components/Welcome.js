218daa8100684fb030ab2874a727f11c2d4d4517
missvader <vanepalazon611@gmail.com>
2022-11-21 15:38:24 +0100

    install react-router-dom and create Welcome component

---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Welcome(){
+  return(
+    <div>
+      <h1>Funciona el routing, esta es la welcome page</h1>
+    </div>
+  )
+}
+export default Welcome;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //elimino el react strict mode pq cada vez que renderiza lee un array vacio y lo establece
+//instalamos react-router-dom, importamos BrowserRouter
 root.render(
-  
+  <BrowserRouter>
     <App />
-  
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
