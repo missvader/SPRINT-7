e1c158d88259c5e92339b20bc69f61adadd13fa5
missvader <vanepalazon611@gmail.com>
2022-11-21 15:53:53 +0100

    Link to navigate to routes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 function Welcome(){
   return(
     <div>
       <h1>Funciona el routing, esta es la welcome page</h1>
+      <Link to="budget">Click si quieres un presupuesto</Link>
     </div>
   )
 }
