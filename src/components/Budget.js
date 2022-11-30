7e117e9405495103e536d46a0fa0c98697a60acc
missvader <vanepalazon611@gmail.com>
2022-11-30 20:59:11 +0100

    localStorage is working perfectly and added date to budget.

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,20 +1,24 @@
 import React from "react";
+/*import {useState} from "react";*/
 import "../App.css";
-function Budget(){
+function Budget(props){
+  //creamos variable para usuarios, estado inicial vacio 
+  /*const [users, setUsers ]= useState([]);
+  console.log(users)*/
   return(
    <div className="container row justify-content-center align-items-center  ">
     <div className="card col-md-6 ">
       <h1 className="card-title text-center">PRESUPUESTO</h1>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Cliente:</li>
-        <li class="list-group-item">Presupuesto:</li>
-        <li class="list-group-item">Fecha:</li>
-        <li class="list-group-item">Página web</li>
-        <li class="list-group-item">Num pag</li>
-        <li class="list-group-item">Num lang</li>
-        <li class="list-group-item">Consultoria SEO</li>
-        <li class="list-group-item">Campaña Google Adds</li>
-        <li class="list-group-item">TOTAL:</li>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Cliente:</li>
+        <li className="list-group-item">Presupuesto:</li>
+        <li className="list-group-item">Fecha:</li>
+        <li className="list-group-item">Página web</li>
+        <li className="list-group-item">Num pag</li>
+        <li className="list-group-item">Num lang</li>
+        <li className="list-group-item">Consultoria SEO</li>
+        <li className="list-group-item">Campaña Google Adds</li>
+        <li className="list-group-item">TOTAL:</li>
       </ul>
     </div>
    </div> 
